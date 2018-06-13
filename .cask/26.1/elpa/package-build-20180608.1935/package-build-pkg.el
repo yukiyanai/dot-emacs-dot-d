15f32dabba4525562852490b5f20a5a60209dc8d
Yuki Yanai <yanai.yuki@kochi-tech.ac.jp>
2018-06-13 14:50:28 +0900

    delte unnecessary folder

--- .cask/26.1/elpa/package-build-20180608.1935/package-build-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "package-build" "20180608.1935" "Tools for assembling a package archive"
-  '((cl-lib "0.5"))
-  :keywords
-  '("tools"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
